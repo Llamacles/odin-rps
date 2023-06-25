89c278907062934716f4187c7ca3d3714298b0b0
Sam <jsabuyuan@gmail.com>
2023-06-24 23:47:34 -0700

    Create functions to get player and computer choice

--- javacript.js
+++ javacript.js
@@ -1,2 +1,17 @@
 let getComputerChoice = () => Math.floor(Math.random() * 3);
 
+let playerSelection = () => {
+    let input = prompt("Rock Paper Scissors!\nWhat is your selection?");
+    return input.toLowerCase();
+};
+
+let computerSelection = () => {
+    switch (getComputerChoice()) {
+        case 0: 
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+};
